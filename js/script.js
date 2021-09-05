fc277a91fb6a5efc54e4b7f9131d963d1c5c1530
Julia <belyakova@gmail.com>
2021-09-05 15:26:03 +0200

    added input data parser

--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
 'use strict';
 
+const dataElem = document.querySelector('#data');
+const enterBtn = document.querySelector('#btn');
+
+enterBtn.addEventListener('click', (e) => {
+    e.stopPropagation;
+    const userData = dataElem.value;
+    const parsedData = parseToObj(userData);
+    console.log(parsedData);
+});
+
+function parseToObj(data) {
+    const replaceFrom = 'lightning';
+    const replaceTo = '5min';
+    const regex = /\d+/g;
+    let result = [];
+
+    data = data.replace(replaceFrom, replaceTo);
+    let lines = data.split('\n');
+    lines = lines.filter(line => line != '');
+
+    lines.forEach((line, i) => {
+        result.push({
+            id: i,
+            title: line.slice(0, line.search(regex)),
+            duration: parseInt(line.match(regex))
+        });
+    });
+
+    return result;
+}
